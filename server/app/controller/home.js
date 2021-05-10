12a4b37038686bdd8bb0ea733d6eeb70b0e8d6f0
木阳也 <837788353@qq.com>
2021-05-10 08:43:38 +0800

    Full commit

---
+++ server/app/controller/home.js
@@ -0,0 +1,10 @@
+'use strict'
+const Controller = require('egg').Controller
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this
+    console.log('xx')
+    ctx.body = 'hi, egg'
+  }
+}
+module.exports = HomeController
